88729e9e24c7c7838060548012df2ea1e43dfb0f
Daniel Göller <61782325+LelViLamp@users.noreply.github.com>
2022-04-08 10:32:18 +0200

    a1 t4 documented functions

--- assignment1/task4.py
+++ assignment1/task4.py
@@ -3,7 +3,8 @@
 import psycopg2 as pg2
 
 """
-INSTRUCTION TASK 4:
+INSTRUCTION TASK 1.4
+--------------------
 write a small Python3 application. We recommend to use the psycopg2 module (or driver) for 
 Python3 to (a) establish a connection to your local database, (b) execute the queries and 
 retrieve the results, and (c) close the connection to your local database. Your application 
@@ -18,23 +19,55 @@ username = 'dbtutorial'
 password = 'dbpwd1'
 
 # Some helper function
+def print_query(query : str, qid : str = '', offset_line : bool = True):
+    """Prints a SQL query (statement) to the console.
+
+    Prints the given SQL statemnt (query) along with its optional identifier (qid)
+    to the console and can be perceded by an empty line before (offset_line).
+    The output will have the form
+        Executing query [QID]: [QUERY_TEXT]
+    
+    Parameters
+    ----------
+    query : str
+        The statement that is to be executed. This will be printed to the console.
+    qid : str, optional
+        Does query have an identifier (Query ID)? If so, you can have it printed before it.
+        Default is an empty string ''.
+    offset_line : bool, optional
+        Should an empty line be printed before the text of this function? If not, set to false.
+        Default is True.
+    """
+
+    if offset_line: print()
+    print(f"Executing query",
+          f" {qid}" if qid != '' else '',
+          ": ",
+          query,
+          sep='')       
 def print_records(records: list[tuple]):
+    """States the number of elements in the given list and prints all elements.
+
+    Output has the form:
+        42 record[s] (was/were) found:
+        [print all records]
+    
+    Parameters
+    ----------
+    records : list[tuple]
+        The records that are to be printed. Can be None.
+    """
+
     if records is None:
         print("The result list is None.")
         return
     print(f"{len(records)} record",
           "s were" if len(records) != 1 else " was",
           " found",
+          ":" if len(records) != 0 else ".",
           sep="")
     for record in records:
         print(record)
-def state_query(query : str, qid : str = '', offset_line : bool = True):
-    if offset_line: print()
-    print(f"Executing query",
-          f" {qid}" if qid != '' else '',
-          ": ",
-          query,
-          sep='')
 
 # Execute file from shell
 if __name__ == "__main__":
@@ -54,7 +87,7 @@ if __name__ == "__main__":
         }
         for qid in queries:
             query = queries[qid]
-            state_query(query, qid)
+            print_query(query, qid)
             cursor.execute(query)
             records = cursor.fetchall()
             print_records(records)
@@ -64,7 +97,7 @@ if __name__ == "__main__":
         # Q3
         qid = "q3"
         query = "SELECT primaryTitle FROM names, titles WHERE names.birthYear = titles.startYear AND names.primaryName = 'Scarlett Johansson';"
-        state_query(query, qid)
+        print_query(query, qid)
         cursor.execute(query)
         counter = cursor.rowcount
         print(f"We found {counter} film",
